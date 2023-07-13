2d0bf1dc60a46298a0b750423be07affee23d5b7
ChrisSnider <109228776+ChrisSnider@users.noreply.github.com>
2023-07-13 09:54:26 +0100

    Added UI and pinch manager

--- Assets/Scripts/PinchSelect.cs
+++ Assets/Scripts/PinchSelect.cs
@@ -6,29 +6,74 @@ public class PinchSelect : MonoBehaviour
 {
 
     GameObject leftHand;
-    Vector3 leftIndex;
-    Vector3 leftThumb;
-    float dist;
+    GameObject rightHand;
+    Transform leftIndex;
+    Transform leftThumb;
+    Vector3 leftPinchPosition;
+    Transform rightIndex;
+    Transform rightThumb;
+    Vector3 rightPinchPosition;
+    float leftDist;
+    float rightDist;
+    // bool isTrackedLeft = false;
 
-    // Start is called before the first frame update
-    void Start()
+    float drawThresh = 0.1f;
+    float detectThresh = 0.2f;
+
+    public bool leftDrawFlag = false;
+    public bool leftDetectFlag = false;
+    public bool rightDrawFlag = false;
+    public bool rightDetectFlag = false;
+
+    void Start() 
     {
-        
+        leftHand = GameObject.Find("Left Hand Debug Draw Joints");
+        rightHand = GameObject.Find("Right Hand Debug Draw Joints");
+
+        rightIndex = rightHand.transform.Find("IndexTip").GetComponent<Transform>();
+        rightThumb = rightHand.transform.Find("ThumbTip").GetComponent<Transform>();
+
+        leftIndex = leftHand.transform.Find("IndexTip").GetComponent<Transform>();
+        leftThumb = leftHand.transform.Find("ThumbTip").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        leftHand = GameObject.Find("Left Hand Debug Draw Joints");
+        leftDist = Vector3.Distance(leftIndex.position, leftThumb.position);
+        rightDist = Vector3.Distance(rightIndex.position, rightThumb.position);
 
-        if (leftHand != null)
+        leftPinchPosition = Vector3.Lerp(leftIndex.position, leftThumb.position, 0.5f);
+        rightPinchPosition = Vector3.Lerp(rightIndex.position, rightThumb.position, 0.5f);
+
+        setFlags(leftDist, rightDist);
+
+        Debug.Log("Left Detect" + leftDetectFlag);
+        Debug.Log("Left Draw" + leftDrawFlag);
+        Debug.Log("Right Detect" + rightDetectFlag);
+        Debug.Log("Right Draw" + rightDetectFlag);
+    }
+
+    void setFlags(float leftDist, float rightDist) 
+    {
+        if (leftDist == 0 | leftDist > detectThresh)
         {
-            leftIndex = leftHand.transform.Find("IndexTip").GetComponent<Transform>().position;
-            leftThumb = leftHand.transform.Find("ThumbTip").GetComponent<Transform>().position;
+            leftDrawFlag = false;
+            leftDetectFlag = false;
+        } else if (leftDist <= drawThresh) {
+            leftDrawFlag = true;
+        } else {
+            leftDetectFlag = true;
+        }
 
-            dist = Vector3.Distance(leftIndex, leftThumb);
-
-            Debug.Log(dist);
+        if (rightDist == 0 | rightDist > detectThresh)
+        {
+            rightDrawFlag = false;
+            rightDetectFlag = false;
+        } else if (rightDist <= drawThresh) {
+            rightDrawFlag = true;
+        } else {
+            rightDetectFlag = true;
         }
     }
 }
